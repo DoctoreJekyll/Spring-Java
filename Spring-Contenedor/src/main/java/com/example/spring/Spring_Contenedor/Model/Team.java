4b358c62e1da511fd13bd653aaeb1c76e5684148
Joseajierro <jrmar0805@gmail.com>
2025-01-08 17:06:42 +0100

    Spring contenedor project

---
+++ Spring-Contenedor/src/main/java/com/example/spring/Spring_Contenedor/Model/Team.java
@@ -0,0 +1,13 @@
+﻿package com.example.spring.Spring_Contenedor.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Team {
+
+    private String countryName;
+    private int rank;
+
+}
---
+++ Spring-Contenedor/src/main/java/com/example/spring/Spring_Contenedor/SpringContenedorApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.Spring_Contenedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringContenedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringContenedorApplication.class, args);
+	}
+
+}
